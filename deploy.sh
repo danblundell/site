7bc23166531cacd7c257878875cc45e3df59da9a
Dan Blundell <hello@danblundell.com>
2019-05-01 12:59:31 +0100

    fix deployment script now we're using docker for the dev env

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,14 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# remove public if it exists
+rm -R public
+
+# clone the built site as the public directory
+git clone git@github.com:danblundell/danblundell.github.io.git public
+
+# Build the project (its running in a docker container, so run the build there)
+docker exec danblundell_blog hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
